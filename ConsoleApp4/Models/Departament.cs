8011b9cda02096b2b9d1040d48457c1e07f8ef13
shiimerry <shiimerry1@gmail.com>
2024-03-31 22:30:12 +0400

    Finalversion

---
+++ ConsoleApp4/Models/Departament.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4.Models
+{
+    public class Departament
+    {
+        Employee[] _employees;
+        byte _departamentno;
+        public byte DepartamentNo
+        {
+            get { return _departamentno; }
+            set
+            {
+                if (value != null) { _departamentno = value; }
+                else { Console.WriteLine("Departament nomresi bow olamaz"); }
+            }
+        }
+        string _departamentname;
+        public string DepartamentName
+        {
+            get { return _departamentname; }
+            set { _departamentname = value; }
+        }
+        Employee[] employees { get { return _employees; } set { _employees = value; } }
+        public Departament()
+        {
+            employees = new Employee[0];
+        }
+        public Departament(string departamentname, byte departamentno)
+        {
+            employees = new Employee[0];
+            DepartamentName = departamentname;
+            DepartamentNo = departamentno;
+        }
+        public void AddEmployee(Employee employee)
+        {
+            Employee[] newemployee = new Employee[employees.Length + 1];
+            for (int i = 0; i < employees.Length; i++)
+            {
+                newemployee[i] = employees[i];
+            }
+            newemployee[newemployee.Length-1] = employee;
+            employees = newemployee;
+ 
+        }
+        public void ShowEmployeeInfo(int id)
+        {
+            foreach (var employee in employees)
+            {
+                if (employee.Id == id)
+                {
+                    Console.WriteLine($"Name-{employee.Name} || Surname- {employee.Surname} || Age - {employee.Age} || Departament № - {DepartamentNo} || Salary - {employee.Salary} ");
+                    Console.WriteLine("\n|===========================|");
+                }
+                else { Console.WriteLine("Isci tapilmadi"); }
+            }
+        }
+
+        public string GetAllEmployess()
+        {
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($" Employee id -{employee.Id}|| Name-{employee.Name} || Surname- {employee.Surname} || Age - {employee.Age} || Salary-{employee.Salary }\n");
+            }
+            return "";
+        }
+    }
+}
+
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,74 @@
+﻿using ConsoleApp4.Models;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("|=====Welcome=====|");
+            bool exit = false;
+            Departament departament = new Departament();
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("|===Choose operation===|");
+                Console.WriteLine("1-Add Employee");
+                Console.WriteLine("2-Watch All Employes");
+                Console.WriteLine("0-exit");
+                string choice = Console.ReadLine();
+                
+                switch (choice)
+                {
+                    case "1":
+                        Employee neemploye = CreateEmployee();
+                        Console.WriteLine("Employee Yaradildi");
+                        departament.AddEmployee(neemploye);
+                        Console.WriteLine("|================|");
+                        break;
+                    case "2":
+                        Console.WriteLine("|================|");
+                        departament.GetAllEmployess();
+                        break;
+                    case "0":
+                        Console.WriteLine("|================|");
+                        Console.WriteLine("good bye");
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("|================|");
+                        Console.WriteLine("Duzgun secim edin");
+                        break;
+                }
+
+            } while (exit != true);
+        }
+        public static Employee CreateEmployee()
+        {
+            Console.WriteLine("employee departament nomresini daxil edin");
+            byte DepartamentNo = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("employee id -ni daxil edin");
+            byte id = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("employee adini daxil edin");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("employee soyadini daxil edin");
+            string Surname = Console.ReadLine();
+
+            Console.WriteLine("employee Age-ni daxil edin");
+            byte Age = Convert.ToByte(Console.ReadLine());
+
+
+            Console.WriteLine("employee Salarysini daxil edin");
+            float Salary = Convert.ToSingle(Console.ReadLine());
+            Employee newemployee = new Employee(Name,Surname,Age,DepartamentNo,Salary,id);
+            return newemployee;
+        }
+
+    }
+}
+
